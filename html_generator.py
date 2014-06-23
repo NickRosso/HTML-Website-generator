7e678802c91e00a890d231e3930eacb4ae945329
Nick Rosso <nicholasrosso@Nicholass-MacBook-Pro.local>
2014-06-23 14:50:56 -0500

    Added Wx Python Widget
    Must have Wx Python installed http://www.wxpython.org/download.php

--- html_generator.py
+++ html_generator.py
@@ -1,11 +1,16 @@
-import os, webbrowser
+import os, webbrowser, wx
 
-def create_layout():
-	title = raw_input("Type a Page Title:")
-	h2 = raw_input("Type a Paragraph Title:")
-	h1 = raw_input("Type a Company Name:")
-	h3 = raw_input("Type Some Content:")
+def get_info(parent=None, message=''):
+	app = wx.App()
+	dlg = wx.TextEntryDialog(parent, message)
+	dlg.ShowModal()
+	result = dlg.GetValue()
+	dlg.Destroy()
+	app.MainLoop()
+	return result
 
+
+def create_layout(title, h2,h1,h3):
 	with open ('css.css', 'w') as css:
 		css.write("body {margin: 0;padding: 0;font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;color: #444;}")
 		css.write("header {font-size: large; color: #E0E0E0; background-color: #2B2B2B;height: 35px;width: 100%;opacity: .9;margin-bottom: 10px; text-align: left}")
@@ -19,8 +24,11 @@ def create_layout():
 	with open ('index.html', 'w') as html:
 		html.write("<!DOCTYPE html><html><head></head><link rel=\"stylesheet\" type =\"text/css\" href=\"css.css\"><title>%s</title><body><header><div class=\"container\"><h1 class=\"logo\">%s</h1></div></header></h1><div class=\"container\"><div class=\"jumbo\"><h2>%s</h2><h3>%s</h3></div></div><p></p></body></html>" % (title, h1,h2,h3))
 		html.close()
-	
-create_layout()
+title = get_info(message = 'Type in Webpage Title: ')
+h1 = get_info(message = 'Type in paragraph Title')
+h2 = get_info(message = 'Type in Company Name')
+h3 = get_info(message = 'Type Some Content')
+create_layout(title, h2, h1, h3)
 webbrowser.open('file://%s' % os.path.abspath('index.html'))
 
 

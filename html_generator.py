ec5473b8634e672d3bb1ec5ba1df560388d21247
Nick Rosso <nicholasrosso@Nicholass-MacBook-Pro.local>
2014-06-23 15:46:52 -0500

    Added Paragraph Element

--- html_generator.py
+++ html_generator.py
@@ -10,7 +10,7 @@ def get_info(parent=None, message=''):
 	return result
 
 
-def create_layout(title, h2,h1,h3):
+def create_layout(title, h2,h1,h3, p):
 	with open ('css.css', 'w') as css:
 		css.write("body {margin: 0;padding: 0;font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;color: #444;}")
 		css.write("header {font-size: large; color: #E0E0E0; background-color: #2B2B2B;height: 35px;width: 100%;opacity: .9;margin-bottom: 10px; text-align: left}")
@@ -19,16 +19,16 @@ def create_layout(title, h2,h1,h3):
 		css.write(".container {width: 940px;margin: 0 auto;}")
 		css.write("h3 {font-size: 1.7em;font-weight: 100;margin-top: 30px;text-align: center;letter-spacing: -1px;color: #999;}")
 		css.write("header h1.logo {margin: 0;font-size: 1.7em;color: #fff;text-transform: uppercase;float: left;}")
+		css.write(".paragraph {background-color: #AAB6BF;height 80%; width 100%;-webkit-border-radius: 6px;-moz-border-radius: 6px;border-radius: 6px;}")
 		css.close()
 
 	with open ('index.html', 'w') as html:
-		html.write("<!DOCTYPE html><html><head></head><link rel=\"stylesheet\" type =\"text/css\" href=\"css.css\"><title>%s</title><body><header><div class=\"container\"><h1 class=\"logo\">%s</h1></div></header></h1><div class=\"container\"><div class=\"jumbo\"><h2>%s</h2><h3>%s</h3></div></div><p></p></body></html>" % (title, h1,h2,h3))
+		html.write("<!DOCTYPE html><html><head></head><link rel=\"stylesheet\" type =\"text/css\" href=\"css.css\"><title>%s</title><body><header><div class=\"container\"><h1 class=\"logo\">%s</h1></div></header></h1><div class=\"container\"><div class=\"jumbo\"><h2>%s</h2><h3>%s</h3></div></div><p class=\"paragraph\">%s</p></body></html>" % (title, h1,h2,h3,p))
 		html.close()
 title = get_info(message = 'Type in Webpage Title: ')
-h1 = get_info(message = 'Type in paragraph Title')
-h2 = get_info(message = 'Type in Company Name')
-h3 = get_info(message = 'Type Some Content')
-create_layout(title, h2, h1, h3)
+h1 = get_info(message = 'Type in Company Title')
+h2 = get_info(message = 'Type in Header Name')
+h3 = get_info(message = 'Type A Description')
+p = get_info(message = 'Type a Paragraph')
+create_layout(title, h2, h1, h3,p)
 webbrowser.open('file://%s' % os.path.abspath('index.html'))
-
-
